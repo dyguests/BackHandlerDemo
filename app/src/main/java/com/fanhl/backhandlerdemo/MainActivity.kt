0f99d3a42c839b2bb569bab6f2209efaa6a28509
fanhl <dyguests@qq.com>
2024-05-18 16:28:08 +0800

    BackHandler works.

--- app/src/main/java/com/fanhl/backhandlerdemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/backhandlerdemo/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Scaffold
@@ -16,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fanhl.backhandlerdemo.ui.theme.BackHandlerDemoTheme
@@ -37,16 +40,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(innerPadding: PaddingValues) {
     var expand by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier.padding(innerPadding),
+        contentAlignment = Alignment.Center,
+    ) {
+        Button(onClick = { expand = true }) {
+            Text(text = "Expand")
+        }
 
-    Button(onClick = { expand = true }) {
-        Text(text = "Expand")
+        DropdownMenu(expanded = expand, onDismissRequest = { expand = false }) {
+            Text(text = "Content")
+        }
+
+        BackHandler(expand) { expand = false }
     }
-
-    DropdownMenu(expanded = expand, onDismissRequest = { expand = false }) {
-        Text(text = "Content")
-    }
-
-    BackHandler(expand) { expand = false }
 }
 
 @Preview(showBackground = true)
